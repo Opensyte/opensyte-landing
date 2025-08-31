3d7b45e43ea69699f620bd2bdb5bfe45e1501a48
Mohamed Ashraf <mohmed6002090@gmail.com>
2025-08-31 12:18:57 +0300

    add analytics, sitemap generation, and update dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,14 +2,62 @@ import "@/styles/globals.css";
 
 import { type Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 
 import { TRPCReactProvider } from "@/trpc/react";
 import { Toaster } from "@/components/ui/sonner";
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://opensyte.org"),
   title: "Opensyte",
   description: "Manage your business 100x more easily",
   icons: [{ rel: "icon", url: "/icon-white.svg" }],
+  keywords: [
+    "react",
+    "server",
+    "components",
+    "opensyte",
+    "radix",
+    "react.js",
+    "next.js",
+    "business",
+    "management",
+    "saas",
+    "development",
+    "developer",
+    "software",
+  ],
+  authors: [
+    {
+      name: "Opensyte",
+      url: "https://opensyte.org",
+    },
+  ],
+  creator: "Opensyte",
+  publisher: "Opensyte",
+  openGraph: {
+    title: "Opensyte",
+    description: "Manage your business 100x more easily",
+    url: "https://opensyte.org",
+    siteName: "Opensyte",
+    images: [
+      {
+        url: "https://opensyte.org/icon-with-text-white.svg",
+        width: 1200,
+        height: 630,
+        alt: "Opensyte",
+      },
+    ],
+    locale: "en-US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Opensyte",
+    description: "Manage your business 100x more easily",
+    images: ["https://opensyte.org/icon-with-text-white.svg"],
+    creator: "@opensyte",
+  },
 };
 
 const inter = Inter({
@@ -25,6 +73,7 @@ export default function RootLayout({
       <body>
         <TRPCReactProvider>{children}</TRPCReactProvider>
         <Toaster />
+        <Analytics />
       </body>
     </html>
   );
